20d6a896768693a1570b1c7173c6c0b878207d2d
amed92 <ahmed.kouachii@gmail.com>
2018-02-27 15:16:32 +0100

    first message

--- soutenanceBack/src/main/java/com/monapp/SoutenanceBackApplication.java
+++ soutenanceBack/src/main/java/com/monapp/SoutenanceBackApplication.java
@@ -3,6 +3,7 @@ package com.monapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//first modify
 @SpringBootApplication
 public class SoutenanceBackApplication {
 
